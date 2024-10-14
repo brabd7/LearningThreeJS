50f54a9dc9a512034cfc9d0154aff4797e461b30
brabd7 <br.abd7@gmail.com>
2024-10-14 22:03:24 +0200

    .

--- src/objects.js
+++ src/objects.js
@@ -7,8 +7,10 @@ export function addObjects(scene)
 
 function createSquare(scene)
 {
-    const geometry = new THREE.BoxGeometry(1, 1, 1); // Crée un cube de 1x1x1
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0xf11ff }); 
-    const cube = new THREE.Mesh(geometry, material); // Crée le mesh avec géométrie et matériau
-    scene.add(cube); // Ajoute le cube à la scène
+    const cube = new THREE.Mesh(geometry, material); 
+
+    // Ajouter le cube à la scène
+    scene.add(cube);
 }
